fa1ed7eeb25654b5450973e49c8fa1f98e39e621
Phạm Thế An <anptse184524@fpt.edu.vn>
2025-07-10 18:23:14 +0700

    Skelention_axe
    chưa fix dc lỗi truy đuổi

---
+++ Assets/Scripts/Enemy/GroundEnemy/HotZoneCheck.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class HotZoneCheck : MonoBehaviour
+{
+    private Enemy_behaviour enemyParent;
+    private bool inRange;
+    private Animator anim;
+
+    private void Awake()
+    {
+        enemyParent = GetComponentInParent<Enemy_behaviour>();
+        anim = GetComponentInParent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (inRange && !anim.GetCurrentAnimatorStateInfo(0).IsName("Enemy_attack"))
+        {
+            enemyParent.Flip();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            inRange = false;
+            gameObject.SetActive(false);
+            enemyParent.triggerZone.SetActive(true);
+            enemyParent.inRange = false;
+            enemyParent.target = null;  // Clear the player target
+            enemyParent.SelectTarget();  // Select a new patrol point target
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/GroundEnemy/TriggerAreaCheck.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TriggerAreaCheck : MonoBehaviour
+{
+    private Enemy_behaviour enemyParent;
+    private void Awake()
+    {
+        enemyParent = GetComponentInParent<Enemy_behaviour>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            enemyParent.target = collision.transform;
+            enemyParent.inRange = true;
+            enemyParent.hotZone.SetActive(true);
+        }
+    }   
+}
