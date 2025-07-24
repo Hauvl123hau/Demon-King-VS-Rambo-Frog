8eb2bd2bca0f0fab16de15091893688d7623a590
Lương Hồ Tuấn Hậu <haulhtse182936@fpt.edu.vn>
2025-07-24 17:21:14 +0700

    add audio fix map

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0; // Pause the game
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1; // Resume the game
+    }
+    public void Exit()
+    {
+       pauseMenu.SetActive(false);
+        Time.timeScale = 1; // Resume the game
+    }
+}
